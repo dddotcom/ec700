c52a01ba71a3f858e4d0edbcc398fc2f2bf8ccee
dddotcom <dddotcom@gmail.com>
2015-03-16 11:23:47 -0400

    testing scapy framework

---
+++ epoch_2/tutorial/tutorial/spiders/metacritic_spider.py
@@ -0,0 +1,21 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from tutorial.items import MetacriticItem
+
+class MetacriticSpider(BaseSpider):
+	name = "metacritic"
+	allowed_comains = ["metacritic.com"]
+	start_urls = [
+		"http://www.metacritic.com/browse/games/title/pc?page=0"
+	]
+
+	def parse(self, response):
+		hxs = HtmlXPathSelector(response)
+		sites = hxs.select('//li[contains(@class, "product game_product")]/div[@class="product_wrap"]')
+		items = []
+
+		for site in sites:
+			item = MetacriticItem()
+			item['title'] = site.select('div[@class="basic_stat product_title"]/a/text()').extract()
+			items.append(item)
+		return items
